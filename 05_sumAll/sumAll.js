9a35a691ba714755858dca9682f00e2052fe21d9
Alexandru Mosgoreanu <amosgoreanu@gmail.com>
2023-10-17 18:11:54 +0300

    Added solution to exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
 
+const sumAll = function(a, b) {
+
+    if(a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b))
+        return "ERROR";
+    let sum = 0;
+    if(a > b) 
+        [a, b] = [b, a];
+
+    for(let i = a; i <= b; i++)
+        sum += i;
+
+    return sum;
 };
 
 // Do not edit below this line
