f5c0a6aa50be7d4294f03ec5b8adabcdad9b3e9c
Alexandru Mosgoreanu <amosgoreanu@gmail.com>
2023-10-20 21:10:20 +0300

    Added solution to exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const str = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    for(let i = 0; i < str.length / 2; i++)
+        if(str[i] != str[str.length-i-1])
+            return false;
 
+    return true;
 };
 
 // Do not edit below this line
