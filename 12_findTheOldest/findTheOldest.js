56ed782fe345a38e6efd043ab6795755aeaa1aad
Alexandru Mosgoreanu <amosgoreanu@gmail.com>
2023-10-20 21:45:20 +0300

    Added solution to exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let result = {};
+    let max = 0;
+    let age;
+    const currentYear = new Date().getFullYear();
+    arr.forEach(elem => {
+        if(!elem.yearOfDeath)
+            age = currentYear - elem.yearOfBirth;
+        else
+            age = elem.yearOfDeath - elem.yearOfBirth;
 
+        if(age > max) {
+            max = age;
+            result = elem;
+        }
+    });
+
+    return result;
 };
 
 // Do not edit below this line
