1db82f254e9859508c676c6672bf5ab0419dc11b
Alexandru Mosgoreanu <amosgoreanu@gmail.com>
2023-10-20 21:39:17 +0300

    Added solution to exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    return arr.reduce((titles, item) => {
+        return [...titles, item.title];
+    }, []);
 };
 
 // Do not edit below this line
